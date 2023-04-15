0b95038d422a71e61bae6574643d3bddd84359e3
serhiiandriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2023-04-15 10:57:46 +0200

    final

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,6 +2,7 @@ export { fetchCountries };
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const countryCard = document.getElementById('js-info');
 const countryList = document.getElementById('js-list');
+const inputEl = document.getElementById('search-box');
 
 function fetchCountries(e) {
   if (e.target.value.trim()) {
@@ -26,12 +27,16 @@ function makeMarkup(arrayOfCountries) {
         );
       } else if (country.length > 1) {
         countryCard.innerHTML = '';
-
+        countryList.addEventListener('click', clickOnCountry);
         let markup = '';
+        const moreInfo =
+          '<p class="more-info"> Click to open more info about country:<p/>';
+        countryList.insertAdjacentHTML('beforeend', moreInfo);
         country.forEach(({ flags, name }) => {
-          markup += `<li><img width='40px' src=${flags.png} alt=${flags.alt} /><span></span>${name.common}</li>`;
+          markup += `<li> <img width='40px' height="30px" src=${flags.png} alt=${flags.alt} />${name.common}</li>`;
         });
-        countryList.innerHTML = markup;
+
+        countryList.insertAdjacentHTML('beforeend', markup);
       } else {
         countryList.innerHTML = '';
         const { flags, name, capital, population, languages } = country[0];
@@ -40,11 +45,13 @@ function makeMarkup(arrayOfCountries) {
         });
         const markup = `<div class='card'>
   <div class='card-body'>
-  <img width='40px' src=${flags.png} alt=${flags.alt} />   
+  <div class="flex-container"> 
+  <img width='40px' height="30px" src=${flags.png} alt=${flags.alt} />   
     <h2  class='card-name'> ${name.common}</h2>
-    <p class='card-info'>Capital: ${capital}</p>
-    <p class='card-info'>Population: ${population}</p>
-    <p class='card-info'>Languages:${lang}</p>
+     </div>
+    <p class='card-info'><span class="key-js">Capital</span>: ${capital}</p>
+    <p class='card-info'><span class="key-js">Population</span>: ${population}</p>
+    <p class='card-info'><span class="key-js">Languages</span>:${lang}</p>
   </div>
 
 </div>`;
@@ -54,4 +61,19 @@ function makeMarkup(arrayOfCountries) {
     .catch(error => {
       Notify.failure('Oops, there is no country with that name');
     });
+}
+function clickOnCountry(event) {
+  window.removeEventListener('click', clickOnCountry);
+  const test = event.target;
+  inputEl.value = '';
+
+  if (test.localName === 'li') {
+    console.log(test.localName);
+    let feltchCountry = fetch(
+      `https://restcountries.com/v3.1/name/${test.textContent.trim()}?fullText=true&fields=languages,capital,name,flags,population`
+    ).then(response => {
+      return response.json();
+    });
+    makeMarkup(feltchCountry);
+  }
 }
